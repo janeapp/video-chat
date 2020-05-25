2ebad9774d3c9f214b8ede656296a9cf88060ba5
Ivan Jiang <>
2020-05-24 23:11:31 -0700

    remove unused code and update the appointment start timestamp condition

--- react/features/conference/components/web/Conference.js
+++ react/features/conference/components/web/Conference.js
@@ -196,7 +196,6 @@ class Conference extends AbstractConference<Props, *> {
                 onMouseMove = { this._onShowToolbar }>
 
                 <Notice />
-                {/*<RefreshButton />*/}
                 <Subject />
                 <div id = 'videospace'>
                     <LargeVideo />
