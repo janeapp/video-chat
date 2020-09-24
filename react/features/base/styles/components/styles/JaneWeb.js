43af56917959b2eec708db1f51feac6fbe847338
Ivan Jiang <>
2020-09-24 13:18:58 -0700

    UI update for the prejoin dialog box and button
    add jane video chat logo,
    add new jsCoreDateCreator function for fixing the moment "invalid" date issue.
    add ActionButton, DialogBox components and rewrite the Prejoin component.

--- react/features/base/styles/components/styles/JaneWeb.js
+++ react/features/base/styles/components/styles/JaneWeb.js
@@ -1,7 +1,21 @@
 export const JaneWeb = {
-  h3: {
-    fontSize: 24,
-    fontFamily: "ProximaNova-Light",
-    lineHeight: 26, // 1.1
-  }
-}
+    h3: {
+        fontSize: 24,
+        fontFamily: 'ProximaNova-Light',
+        lineHeight: 26 // 1.1
+    },
+    light: {
+        fontFamily: 'ProximaNova-Light'
+    },
+    bold: {
+        fontFamily: 'ProximaNova-Bold'
+    },
+    regular: {
+        fontFamily: 'ProximaNova-Bold',
+        fontWeight: '400'
+    },
+    semiBold: {
+        fontFamily: 'ProximaNova-Bold',
+        fontWeight: '600'
+    }
+};
