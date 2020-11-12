21147949263388a146bbd743ac34292be35ef030
Ivan Jiang <>
2020-11-11 21:31:54 -0800

    new action to control the visibility of the pre call info message and to change the margin top of the filmstrip.
    new action to update the jwt auth info.
    
    add isJaneTestMode function for detecting the user is on the test my setup stage

--- react/features/base/conference/functions.js
+++ react/features/base/conference/functions.js
@@ -22,6 +22,7 @@ import {
 } from './constants';
 import logger from './logger';
 
+import jwtDecode from 'jwt-decode';
 /**
  * Attach a set of local tracks to a conference.
  *
@@ -369,4 +370,16 @@ function safeStartCase(s = '') {
     return _.words(`${s}`.replace(/['\u2019]/g, '')).reduce(
         (result, word, index) => result + (index ? ' ' : '') + _.upperFirst(word)
         , '');
+}
+
+export function isJaneTestMode(state: Object) {
+    const { jwt } = state['features/base/jwt'];
+    const jwtPayload = jwt && jwtDecode(jwt) || null;
+    const context = jwtPayload && jwtPayload.context || null;
+    const user = context && context.user || null;
+    const participantId = user && user.participant_id;
+    const videoChatSessionId = context && context.video_chat_session_id;
+    const participantEmail = user && user.email;
+
+    return participantId === 0 && videoChatSessionId === 0 && participantEmail === 'test@test.com';
 }
