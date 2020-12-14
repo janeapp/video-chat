fbc6def0e56271ba01a021e7cbda2dd6a4136ed8
Ivan Jiang <>
2020-12-14 14:07:02 -0800

    update the web view onerror event handler.
    fix the jwt invalid token error bug when participant click on the hangup button on the waiting area page.

--- react/features/toolbox/components/HangupButton.js
+++ react/features/toolbox/components/HangupButton.js
@@ -68,10 +68,13 @@ class HangupButton extends AbstractHangupButton<Props, *> {
     }
 }
 
+
 // eslint-disable-next-line require-jsdoc
 function mapStateToProps(state): Object {
+    const { jwt } = state['features/base/jwt'];
+
     return {
-        jwt: state['features/base/jwt']
+        jwt
     };
 }
 
