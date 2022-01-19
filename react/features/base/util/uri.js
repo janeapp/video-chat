7f2425bb247f2d52b2d409a94d67ff3c9568d78d
Ivan Jiang <ivanjiang5628@gmail.com>
2022-01-19 09:57:41 -0800

    add new universal link domains to the ios app.

--- react/features/base/util/uri.js
+++ react/features/base/util/uri.js
@@ -40,12 +40,22 @@ const _URI_PATH_PATTERN = '([^?#]*)';
  * "videochat-ca2.janeapp.com",
  * "videochat.janeapp.com.au",
  * "videochat.janeapp.co.uk";
+ * 'videochat-chrisw.jane.qa',
+ * 'jitsi2.jane.qa',
+ * 'conference-pod-cac1-j1.janeapp.net',
+ * 'conference-pod-usw2-j2.janeapp.net',
+ * 'conference-pod-euw2-j3.janeapp.net',
+ * 'conference-pod-apse2-j4.janeapp.net',
+ * 'video-conference-ca.janeapp.net',
+ * 'video-conference-us.janeapp.net',
+ * 'video-conference-uk.janeapp.net',
+ * 'video-conference-au.janeapp.net',
+ * 'video-conference.jane.qa'
  *
  * @private
  * @type {string}
  */
-// eslint-disable-next-line max-len,no-useless-escape
-const _JANE_UNIVERSAL_LINK_DOMAINS = /videochat(?:\-(?:(?:ca2?|us)\.janeapp\.com|jwt|chrisw\.jane\.qa)|\.janeapp\.co(?:m\.au|\.uk))/;
+const _JANE_UNIVERSAL_LINK_DOMAINS = '([a-z0-9]+[.])*(jane|janeapp).(qa|com|com.au|co.uk|net)';
 
 /**
  * The {@link RegExp} pattern of the protocol of a URI.
