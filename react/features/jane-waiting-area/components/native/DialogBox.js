9c5ad803a05d1be76e494af810b8d6b24d3008ce
Kae Ramirez <88002018+kaeramirez@users.noreply.github.com>
2023-11-07 13:48:57 -0800

    Update iOS UI

--- react/features/jane-waiting-area/components/native/DialogBox.js
+++ react/features/jane-waiting-area/components/native/DialogBox.js
@@ -46,7 +46,8 @@ type DialogBoxProps = {
     remoteParticipantsStatuses: Array<Object>,
     authState: string,
     localParticipantCanJoin: boolean,
-    t: Function
+    t: Function,
+    janeAppointmentDetails: Object
 };
 
 type SocketWebViewProps = {
@@ -245,8 +246,8 @@ class DialogBox extends Component<DialogBoxProps> {
     }
 
     _getStartDate() {
-        const { jwtPayload } = this.props;
-        const startAt = _.get(jwtPayload, 'context.start_at') ?? '';
+        const { janeAppointmentDetails } = this.props;
+        const startAt = _.get(janeAppointmentDetails, 'start_at') ?? '';
 
         if (startAt) {
             return (<Text style = { styles.msgText }>
@@ -261,10 +262,10 @@ class DialogBox extends Component<DialogBoxProps> {
     }
 
     _getStartTimeAndEndTime() {
-        const { jwtPayload } = this.props;
-        const startAt = _.get(jwtPayload, 'context.start_at') ?? '';
-        let endAt = _.get(jwtPayload, 'context.end_at') ?? '';
-        const treatmentDuration = Number(_.get(jwtPayload, 'context.treatment_duration'));
+        const { janeAppointmentDetails } = this.props;
+        const startAt = _.get(janeAppointmentDetails, 'start_at') ?? '';
+        let endAt = _.get(janeAppointmentDetails, 'end_at') ?? '';
+        const treatmentDuration = _.get(janeAppointmentDetails, 'treatment_duration');
 
         if (!startAt || !endAt) {
             return null;
@@ -285,10 +286,10 @@ class DialogBox extends Component<DialogBoxProps> {
     }
 
     _getDuration() {
-        const { jwtPayload, t } = this.props;
-        const startAt = _.get(jwtPayload, 'context.start_at');
-        const endAt = _.get(jwtPayload, 'context.end_at');
-        const treatmentDuration = _.get(jwtPayload, 'context.treatment_duration');
+        const { janeAppointmentDetails, t } = this.props;
+        const startAt = _.get(janeAppointmentDetails, 'start_at');
+        const endAt = _.get(janeAppointmentDetails, 'end_at');
+        const treatmentDuration = _.get(janeAppointmentDetails, 'treatment_duration');
         let duration;
 
         if (treatmentDuration) {
@@ -367,11 +368,11 @@ class DialogBox extends Component<DialogBoxProps> {
     render() {
         const {
             participantType,
-            jwtPayload,
             locationURL,
             localParticipantCanJoin,
             authState,
-            t
+            t,
+            janeAppointmentDetails
         } = this.props;
 
         return (<View style = { styles.janeWaitingAreaContainer }>
@@ -400,12 +401,13 @@ class DialogBox extends Component<DialogBoxProps> {
                         <View style = { styles.infoDetailContainer }>
                             <Text style = { [ styles.msgText, styles.boldText ] }>
                                 {
-                                    jwtPayload && jwtPayload.context && jwtPayload.context.treatment
+                                    janeAppointmentDetails && janeAppointmentDetails.treatment
                                 }
                             </Text>
                             <Text style = { [ styles.msgText, styles.boldText ] }>
                                 {
-                                    jwtPayload && jwtPayload.context && jwtPayload.context.practitioner_name
+
+                                    janeAppointmentDetails && janeAppointmentDetails.practitioner_name
                                 }
                             </Text>
                             {
@@ -465,7 +467,7 @@ function mapStateToProps(state): Object {
     const participant = getLocalParticipantFromJwt(state);
     const participantType = getLocalParticipantType(state);
     const { locationURL } = state['features/base/connection'];
-    const { remoteParticipantsStatuses, authState } = state['features/jane-waiting-area'];
+    const { remoteParticipantsStatuses, authState, janeAppointmentDetails } = state['features/jane-waiting-area'];
     const localParticipantCanJoin = checkLocalParticipantCanJoin(state);
 
     return {
@@ -476,7 +478,8 @@ function mapStateToProps(state): Object {
         locationURL,
         remoteParticipantsStatuses,
         authState,
-        localParticipantCanJoin
+        localParticipantCanJoin,
+        janeAppointmentDetails
     };
 }
 
